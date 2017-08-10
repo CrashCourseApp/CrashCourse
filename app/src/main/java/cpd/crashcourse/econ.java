69d577faf0a06346000d20c120295289988a2600
Aman <amankishore105@gmail.com>
2017-08-10 18:38:12 -0400

    FInished all of Econ

--- app/src/main/java/cpd/crashcourse/econ.java
+++ app/src/main/java/cpd/crashcourse/econ.java
@@ -1,5 +1,6 @@
 package cpd.crashcourse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,158 @@ public class econ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_econ);
-
     }
+
+    public void goToEcon1(View v){
+        Intent intent = new Intent(this, econ1.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon2(View v){
+        Intent intent = new Intent(this, econ2.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon3(View v){
+        Intent intent = new Intent(this, econ3.class);
+        startActivity(intent);
+    }
+    public void goToEcon4(View v){
+        Intent intent = new Intent(this, econ4.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon5(View v){
+        Intent intent = new Intent(this, econ5.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon6(View v){
+        Intent intent = new Intent(this, econ6.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon7(View v){
+        Intent intent = new Intent(this, econ7.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon8(View v){
+        Intent intent = new Intent(this, econ8.class);
+        startActivity(intent);
+    }
+    public void goToEcon9(View v){
+        Intent intent = new Intent(this, econ9.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon10(View v){
+        Intent intent = new Intent(this, econ10.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon11(View v){
+        Intent intent = new Intent(this, econ11.class);
+        startActivity(intent);
+    }
+    public void goToEcon12(View v){
+        Intent intent = new Intent(this, econ12.class);
+        startActivity(intent);
+    }
+    public void goToEcon13(View v){
+        Intent intent = new Intent(this, econ13.class);
+        startActivity(intent);
+    }
+    public void goToEcon14(View v){
+        Intent intent = new Intent(this, econ14.class);
+        startActivity(intent);
+    }
+    public void goToEcon15(View v){
+        Intent intent = new Intent(this, econ15.class);
+        startActivity(intent);
+    }
+    public void goToEcon16(View v){
+        Intent intent = new Intent(this, econ16.class);
+        startActivity(intent);
+    }
+    public void goToEcon17(View v){
+        Intent intent = new Intent(this, econ17.class);
+        startActivity(intent);
+    }
+    public void goToEcon18(View v){
+        Intent intent = new Intent(this, econ18.class);
+        startActivity(intent);
+    }
+    public void goToEcon19(View v){
+        Intent intent = new Intent(this, econ19.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon20(View v){
+        Intent intent = new Intent(this, econ20.class);
+        startActivity(intent);
+    }
+    public void goToEcon21(View v){
+        Intent intent = new Intent(this, econ21.class);
+        startActivity(intent);
+    }
+    public void goToEcon22(View v){
+        Intent intent = new Intent(this, econ22.class);
+        startActivity(intent);
+    }
+    public void goToEcon23(View v){
+        Intent intent = new Intent(this, econ23.class);
+        startActivity(intent);
+    }
+    public void goToEcon24(View v){
+        Intent intent = new Intent(this, econ24.class);
+        startActivity(intent);
+    }
+    public void goToEcon25(View v){
+        Intent intent = new Intent(this, econ25.class);
+        startActivity(intent);
+    }
+    public void goToEcon26(View v){
+        Intent intent = new Intent(this, econ26.class);
+        startActivity(intent);
+    }
+    public void goToEcon27(View v){
+        Intent intent = new Intent(this, econ27.class);
+        startActivity(intent);
+    }
+    public void goToEcon28(View v){
+        Intent intent = new Intent(this, econ28.class);
+        startActivity(intent);
+    }
+    public void goToEcon29(View v){
+        Intent intent = new Intent(this, econ29.class);
+        startActivity(intent);
+    }
+
+    public void goToEcon30(View v){
+        Intent intent = new Intent(this, econ30.class);
+        startActivity(intent);
+    }
+    public void goToEcon31(View v){
+        Intent intent = new Intent(this, econ31.class);
+        startActivity(intent);
+    }
+    public void goToEcon32(View v){
+        Intent intent = new Intent(this, econ32.class);
+        startActivity(intent);
+    }
+    public void goToEcon33(View v){
+        Intent intent = new Intent(this, econ33.class);
+        startActivity(intent);
+    }
+    public void goToEcon34(View v){
+        Intent intent = new Intent(this, econ34.class);
+        startActivity(intent);
+    }
+    public void goToEcon35(View v){
+        Intent intent = new Intent(this, econ35.class);
+        startActivity(intent);
+    }
+
 }
+
