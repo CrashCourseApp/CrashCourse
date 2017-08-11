1ade529108dabda0130f75df5934e061e5976506
cdeshpande17 <cdeshpande17@gmail.com>
2017-08-11 14:38:49 -0400

    Large Push

--- app/src/main/java/cpd/crashcourse/BigHistory.java
+++ app/src/main/java/cpd/crashcourse/BigHistory.java
@@ -1,5 +1,6 @@
 package cpd.crashcourse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,5 +15,47 @@ public class BigHistory extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_big_history);
 
+    }
+
+    public void goToH1(View v){
+        Intent intent = new Intent(this, h1.class);
+        startActivity(intent);
+    }
+
+    public void goToH2(View v){
+        Intent intent = new Intent(this, h2.class);
+        startActivity(intent);
+    }
+    public void goToH3(View v){
+        Intent intent = new Intent(this, FullscreenActivity.class);
+        startActivity(intent);
+    }
+    public void goToH4(View v){
+        Intent intent = new Intent(this, h4.class);
+        startActivity(intent);
+    }
+    public void goToH5(View v){
+        Intent intent = new Intent(this, h5.class);
+        startActivity(intent);
+    }
+    public void goToH6(View v){
+        Intent intent = new Intent(this, h6.class);
+        startActivity(intent);
+    }
+    public void goToH7(View v){
+        Intent intent = new Intent(this, h7.class);
+        startActivity(intent);
+    }
+    public void goToH8(View v){
+        Intent intent = new Intent(this, h8.class);
+        startActivity(intent);
+    }
+    public void goToH9(View v){
+        Intent intent = new Intent(this, h9.class);
+        startActivity(intent);
+    }
+    public void goToH10(View v){
+        Intent intent = new Intent(this, h10.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/cpd/crashcourse/BiologyPage.java
+++ app/src/main/java/cpd/crashcourse/BiologyPage.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
 
 public class BiologyPage extends AppCompatActivity {
 
@@ -15,4 +16,178 @@ public class BiologyPage extends AppCompatActivity {
         setContentView(R.layout.activity_biology_page);
 
     }
+
+    public void goToBio1(View v){
+        Intent intent = new Intent(this, bio1.class);
+        startActivity(intent);
+    }
+
+    public void goToBio2(View v){
+        Intent intent = new Intent(this, bio2.class);
+        startActivity(intent);
+    }
+    public void goToBio3(View v){
+        Intent intent = new Intent(this, bio3.class);
+        startActivity(intent);
+    }
+    public void goToBio4(View v){
+        Intent intent = new Intent(this, bio4.class);
+        startActivity(intent);
+    }
+    public void goToBio5(View v){
+        Intent intent = new Intent(this, bio5.class);
+        startActivity(intent);
+    }
+    public void goToBio6(View v){
+        Intent intent = new Intent(this, bio6.class);
+        startActivity(intent);
+    }
+    public void goToBio7(View v){
+        Intent intent = new Intent(this, bio7.class);
+        startActivity(intent);
+    }
+    public void goToBio8(View v){
+        Intent intent = new Intent(this, bio8.class);
+        startActivity(intent);
+    }
+    public void goToBio9(View v){
+        Intent intent = new Intent(this, bio9.class);
+        startActivity(intent);
+    }
+    public void goToBio10(View v){
+        Intent intent = new Intent(this, bio10.class);
+        startActivity(intent);
+    }
+
+    public void goToBio11(View v){
+        Intent intent = new Intent(this, bio11.class);
+        startActivity(intent);
+    }
+
+    public void goToBio12(View v){
+        Intent intent = new Intent(this, bio12.class);
+        startActivity(intent);
+    }
+    public void goToBio13(View v){
+        Intent intent = new Intent(this, bio13.class);
+        startActivity(intent);
+    }
+    public void goToBio14(View v){
+        Intent intent = new Intent(this, bio14.class);
+        startActivity(intent);
+    }
+    public void goToBio15(View v){
+        Intent intent = new Intent(this, bio15.class);
+        startActivity(intent);
+    }
+    public void goToBio16(View v){
+        Intent intent = new Intent(this, bio16.class);
+        startActivity(intent);
+    }
+    public void goToBio17(View v){
+        Intent intent = new Intent(this, bio17.class);
+        startActivity(intent);
+    }
+    public void goToBio18(View v){
+        Intent intent = new Intent(this, bio18.class);
+        startActivity(intent);
+    }
+    public void goTobio19(View v){
+        Intent intent = new Intent(this, bio19.class);
+        startActivity(intent);
+    }
+    public void goTobio20(View v){
+        Intent intent = new Intent(this, bio20.class);
+        startActivity(intent);
+    }
+    public void goToBio21(View v){
+        Intent intent = new Intent(this, bio21.class);
+        startActivity(intent);
+    }
+
+    public void goToBio22(View v){
+        Intent intent = new Intent(this, bio22.class);
+        startActivity(intent);
+    }
+    public void goToBio23(View v){
+        Intent intent = new Intent(this, FullscreenActivity.class);
+        startActivity(intent);
+    }
+    public void goToBio24(View v){
+        Intent intent = new Intent(this, bio24.class);
+        startActivity(intent);
+    }
+    public void goToBio25(View v){
+        Intent intent = new Intent(this, bio25.class);
+        startActivity(intent);
+    }
+    public void goToBio26(View v){
+        Intent intent = new Intent(this, bio26.class);
+        startActivity(intent);
+    }
+    public void goToBio27(View v){
+        Intent intent = new Intent(this, bio27.class);
+        startActivity(intent);
+    }
+    public void goToBio28(View v){
+        Intent intent = new Intent(this, bio28.class);
+        startActivity(intent);
+    }
+    public void goToBio29(View v){
+        Intent intent = new Intent(this, bio29.class);
+        startActivity(intent);
+    }
+    public void goToBio30(View v){
+        Intent intent = new Intent(this, bio30.class);
+        startActivity(intent);
+    }
+    public void goToBio31(View v){
+        Intent intent = new Intent(this, bio31.class);
+        startActivity(intent);
+    }
+
+    public void goToBio32(View v){
+        Intent intent = new Intent(this, bio32.class);
+        startActivity(intent);
+    }
+    public void goToBio33(View v){
+        Intent intent = new Intent(this, bio33.class);
+        startActivity(intent);
+    }
+    public void goToBio34(View v){
+        Intent intent = new Intent(this, bio34.class);
+        startActivity(intent);
+    }
+    public void goToBio35(View v){
+        Intent intent = new Intent(this, bio35.class);
+        startActivity(intent);
+    }
+    public void goToBio36(View v){
+        Intent intent = new Intent(this, bio36.class);
+        startActivity(intent);
+    }
+    public void goToBio37(View v){
+        Intent intent = new Intent(this, bio37.class);
+        startActivity(intent);
+    }
+    public void goToBio38(View v){
+        Intent intent = new Intent(this, bio38.class);
+        startActivity(intent);
+    }
+    public void goToBio39(View v){
+        Intent intent = new Intent(this, bio39.class);
+        startActivity(intent);
+    }
+    public void goToBio40(View v){
+        Intent intent = new Intent(this, bio40.class);
+        startActivity(intent);
+    }
+
 }
+
+
+
+
+
+
+
