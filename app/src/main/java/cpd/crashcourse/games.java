43cf1e2adbc2400eb555ed3755993a76c212c8ae
cdeshpande17 <cdeshpande17@gmail.com>
2017-08-14 13:56:38 -0400

    Large Push v2

--- app/src/main/java/cpd/crashcourse/games.java
+++ app/src/main/java/cpd/crashcourse/games.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
 
 public class games extends AppCompatActivity {
 
@@ -14,5 +15,150 @@ public class games extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
 
+    }
+
+    public void goTogames1(View v) {
+        Intent intent = new Intent(this, games1.class);
+        startActivity(intent);
+    }
+
+    public void goTogames2(View v) {
+        Intent intent = new Intent(this, games2.class);
+        startActivity(intent);
+    }
+
+    public void goTogames3(View v) {
+        Intent intent = new Intent(this, games3.class);
+        startActivity(intent);
+    }
+
+    public void goTogames4(View v) {
+        Intent intent = new Intent(this, games4.class);
+        startActivity(intent);
+    }
+
+    public void goTogames5(View v) {
+        Intent intent = new Intent(this, games5.class);
+        startActivity(intent);
+    }
+
+    public void goTogames6(View v) {
+        Intent intent = new Intent(this, games6.class);
+        startActivity(intent);
+    }
+
+    public void goTogames7(View v) {
+        Intent intent = new Intent(this, games7.class);
+        startActivity(intent);
+    }
+
+    public void goTogames8(View v) {
+        Intent intent = new Intent(this, games8.class);
+        startActivity(intent);
+    }
+
+    public void goTogames9(View v) {
+        Intent intent = new Intent(this, games9.class);
+        startActivity(intent);
+    }
+
+    public void goTogames10(View v) {
+        Intent intent = new Intent(this, games10.class);
+        startActivity(intent);
+    }
+
+    public void goTogames11(View v) {
+        Intent intent = new Intent(this, games11.class);
+        startActivity(intent);
+    }
+
+    public void goTogames12(View v) {
+        Intent intent = new Intent(this, games12.class);
+        startActivity(intent);
+    }
+
+    public void goTogames13(View v) {
+        Intent intent = new Intent(this, games13.class);
+        startActivity(intent);
+    }
+
+    public void goTogames14(View v) {
+        Intent intent = new Intent(this, games14.class);
+        startActivity(intent);
+    }
+
+    public void goTogames15(View v) {
+        Intent intent = new Intent(this, games15.class);
+        startActivity(intent);
+    }
+
+    public void goTogames16(View v) {
+        Intent intent = new Intent(this, games16.class);
+        startActivity(intent);
+    }
+
+    public void goTogames17(View v) {
+        Intent intent = new Intent(this, games17.class);
+        startActivity(intent);
+    }
+
+    public void goTogames18(View v) {
+        Intent intent = new Intent(this, games18.class);
+        startActivity(intent);
+    }
+
+    public void goTogames19(View v) {
+        Intent intent = new Intent(this, games19.class);
+        startActivity(intent);
+    }
+
+    public void goTogames20(View v) {
+        Intent intent = new Intent(this, games20.class);
+        startActivity(intent);
+    }
+
+    public void goTogames21(View v) {
+        Intent intent = new Intent(this, games21.class);
+        startActivity(intent);
+    }
+
+    public void goTogames22(View v) {
+        Intent intent = new Intent(this, games22.class);
+        startActivity(intent);
+    }
+
+    public void goTogames23(View v) {
+        Intent intent = new Intent(this, FullscreenActivity.class);
+        startActivity(intent);
+    }
+
+    public void goTogames24(View v) {
+        Intent intent = new Intent(this, games24.class);
+        startActivity(intent);
+    }
+
+    public void goTogames25(View v) {
+        Intent intent = new Intent(this, games25.class);
+        startActivity(intent);
+    }
+
+    public void goTogames26(View v) {
+        Intent intent = new Intent(this, games26.class);
+        startActivity(intent);
+    }
+
+    public void goTogames27(View v) {
+        Intent intent = new Intent(this, games27.class);
+        startActivity(intent);
+    }
+
+    public void goTogames28(View v) {
+        Intent intent = new Intent(this, games28.class);
+        startActivity(intent);
+    }
+
+    public void goTogames29(View v) {
+        Intent intent = new Intent(this, games29.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/cpd/crashcourse/intellectualproperty.java
+++ app/src/main/java/cpd/crashcourse/intellectualproperty.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
 
 public class intellectualproperty extends AppCompatActivity {
 
@@ -14,6 +15,40 @@ public class intellectualproperty extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intellectualproperty);
 
+    }
 
+    public void goToip1(View v) {
+        Intent intent = new Intent(this, ip1.class);
+        startActivity(intent);
+    }
+
+    public void goToip2(View v) {
+        Intent intent = new Intent(this, ip2.class);
+        startActivity(intent);
+    }
+
+    public void goToip3(View v) {
+        Intent intent = new Intent(this, ip3.class);
+        startActivity(intent);
+    }
+
+    public void goToip4(View v) {
+        Intent intent = new Intent(this, ip4.class);
+        startActivity(intent);
+    }
+
+    public void goToip5(View v) {
+        Intent intent = new Intent(this, ip5.class);
+        startActivity(intent);
+    }
+
+    public void goToip6(View v) {
+        Intent intent = new Intent(this, ip6.class);
+        startActivity(intent);
+    }
+
+    public void goToip7(View v) {
+        Intent intent = new Intent(this, ip7.class);
+        startActivity(intent);
     }
 }
