7915b92d24a6e01114749f984f70454c5e5d17d0
cdeshpande17 <cdeshpande17@gmail.com>
2017-08-17 01:51:30 -0400

    fix

--- app/src/main/java/cpd/crashcourse/literature.java
+++ app/src/main/java/cpd/crashcourse/literature.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
 
 public class literature extends AppCompatActivity {
 
@@ -15,4 +16,124 @@ public class literature extends AppCompatActivity {
         setContentView(R.layout.activity_literature);
 
     }
+
+    public void goTolit1(View v){
+        Intent intent = new Intent(this, lit1.class);
+        startActivity(intent);
+    }
+
+    public void goTolit2(View v){
+        Intent intent = new Intent(this, lit2.class);
+        startActivity(intent);
+    }
+    public void goTolit3(View v){
+        Intent intent = new Intent(this, lit3.class);
+        startActivity(intent);
+    }
+    public void goTolit4(View v){
+        Intent intent = new Intent(this, lit4.class);
+        startActivity(intent);
+    }
+    public void goTolit5(View v){
+        Intent intent = new Intent(this, lit5.class);
+        startActivity(intent);
+    }
+    public void goTolit6(View v){
+        Intent intent = new Intent(this, lit6.class);
+        startActivity(intent);
+    }
+    public void goTolit7(View v){
+        Intent intent = new Intent(this, lit7.class);
+        startActivity(intent);
+    }
+    public void goTolit8(View v){
+        Intent intent = new Intent(this, lit8.class);
+        startActivity(intent);
+    }
+    public void goTolit9(View v){
+        Intent intent = new Intent(this, lit9.class);
+        startActivity(intent);
+    }
+    public void goTolit10(View v){
+        Intent intent = new Intent(this, lit10.class);
+        startActivity(intent);
+    }
+
+    public void goTolit11(View v){
+        Intent intent = new Intent(this, lit11.class);
+        startActivity(intent);
+    }
+
+    public void goTolit12(View v){
+        Intent intent = new Intent(this, lit12.class);
+        startActivity(intent);
+    }
+    public void goTolit13(View v){
+        Intent intent = new Intent(this, lit13.class);
+        startActivity(intent);
+    }
+    public void goTolit14(View v){
+        Intent intent = new Intent(this, lit14.class);
+        startActivity(intent);
+    }
+    public void goTolit15(View v){
+        Intent intent = new Intent(this, lit15.class);
+        startActivity(intent);
+    }
+    public void goTolit16(View v){
+        Intent intent = new Intent(this, lit16.class);
+        startActivity(intent);
+    }
+    public void goTolit17(View v){
+        Intent intent = new Intent(this, lit17.class);
+        startActivity(intent);
+    }
+    public void goTolit18(View v){
+        Intent intent = new Intent(this, lit18.class);
+        startActivity(intent);
+    }
+    public void goTolit19(View v){
+        Intent intent = new Intent(this, lit19.class);
+        startActivity(intent);
+    }
+    public void goTolit20(View v){
+        Intent intent = new Intent(this, lit20.class);
+        startActivity(intent);
+    }
+    public void goTolit21(View v){
+        Intent intent = new Intent(this, lit21.class);
+        startActivity(intent);
+    }
+
+    public void goTolit22(View v){
+        Intent intent = new Intent(this, lit22.class);
+        startActivity(intent);
+    }
+    public void goTolit23(View v){
+        Intent intent = new Intent(this, lit23.class);
+        startActivity(intent);
+    }
+    public void goTolit24(View v){
+        Intent intent = new Intent(this, lit24.class);
+        startActivity(intent);
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
