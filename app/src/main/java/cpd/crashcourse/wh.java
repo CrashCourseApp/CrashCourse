f1b2870c762436d18a6b5c1b3fde0de3f73c8448
cdeshpande17 <cdeshpande17@gmail.com>
2017-08-16 22:26:39 -0400

    final app!

--- app/src/main/java/cpd/crashcourse/wh.java
+++ app/src/main/java/cpd/crashcourse/wh.java
@@ -1,7 +1,10 @@
 package cpd.crashcourse;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
 
@@ -10,49 +13,49 @@ public class wh extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wh);
+        setContentView(R.layout.activity_whone);
 
     }
 
     public void goTowhone1(View v){
-        Intent intent = new Intent(this, whtwo1.class);
+        Intent intent = new Intent(this, whone1.class);
         startActivity(intent);
     }
 
     public void goTowhone2(View v){
-        Intent intent = new Intent(this, whtwo2.class);
+        Intent intent = new Intent(this, whone2.class);
         startActivity(intent);
     }
     public void goTowhone3(View v){
-        Intent intent = new Intent(this, whtwo3.class);
+        Intent intent = new Intent(this, whone3.class);
         startActivity(intent);
     }
     public void goTowhone4(View v){
-        Intent intent = new Intent(this, whtwo4.class);
+        Intent intent = new Intent(this, whone4.class);
         startActivity(intent);
     }
     public void goTowhone5(View v){
-        Intent intent = new Intent(this, whtwo5.class);
+        Intent intent = new Intent(this, whone5.class);
         startActivity(intent);
     }
     public void goTowhone6(View v){
-        Intent intent = new Intent(this, whtwo6.class);
+        Intent intent = new Intent(this, whone6.class);
         startActivity(intent);
     }
     public void goTowhone7(View v){
-        Intent intent = new Intent(this, whtwo7.class);
+        Intent intent = new Intent(this, whone7.class);
         startActivity(intent);
     }
     public void goTowhone8(View v){
-        Intent intent = new Intent(this, whtwo8.class);
+        Intent intent = new Intent(this, whone8.class);
         startActivity(intent);
     }
     public void goTowhone9(View v){
-        Intent intent = new Intent(this, whtwo9.class);
+        Intent intent = new Intent(this, whone9.class);
         startActivity(intent);
     }
     public void goTowhone10(View v){
-        Intent intent = new Intent(this, whtwo10.class);
+        Intent intent = new Intent(this, whone10.class);
         startActivity(intent);
     }
 
@@ -138,7 +141,66 @@ public class wh extends AppCompatActivity {
         Intent intent = new Intent(this, whone30.class);
         startActivity(intent);
     }
+    public void goTowhone31(View v){
+        Intent intent = new Intent(this, whone31.class);
+        startActivity(intent);
+    }
+
+    public void goTowhone32(View v){
+        Intent intent = new Intent(this, whone32.class);
+        startActivity(intent);
+    }
+    public void goTowhone33(View v){
+        Intent intent = new Intent(this, whone33.class);
+        startActivity(intent);
+    }
+    public void goTowhone34(View v){
+        Intent intent = new Intent(this, whone34.class);
+        startActivity(intent);
+    }
+    public void goTowhone35(View v){
+        Intent intent = new Intent(this, whone35.class);
+        startActivity(intent);
+    }
+    public void goTowhone36(View v){
+        Intent intent = new Intent(this, whone36.class);
+        startActivity(intent);
+    }
+    public void goTowhone37(View v){
+        Intent intent = new Intent(this, whone37.class);
+        startActivity(intent);
+    }
+    public void goTowhone38(View v){
+        Intent intent = new Intent(this, whone38.class);
+        startActivity(intent);
+    }
+    public void goTowhone39(View v){
+        Intent intent = new Intent(this, whone39.class);
+        startActivity(intent);
+    }
+    public void goTowhone40(View v){
+        Intent intent = new Intent(this, whone40.class);
+        startActivity(intent);
+    }
+
+    public void goTowhone41(View v){
+        Intent intent = new Intent(this, whone41.class);
+        startActivity(intent);
+    }
+
+    public void goTowhone42(View v){
+        Intent intent = new Intent(this, whone42.class);
+        startActivity(intent);
+    }
+
+
 }
+
+
+
+
+
+
 
 
 
